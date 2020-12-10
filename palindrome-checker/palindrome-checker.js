74234a6655add1239307157279406dde1c08c396
asorsz <asorsz@gmail.com>
2020-12-10 15:02:51 +0000

    Create front-end for Palindrome Checker

--- palindrome-checker/palindrome-checker.js
+++ palindrome-checker/palindrome-checker.js
@@ -1,31 +1,63 @@
-function palindrome(str) {
-  
-  var regex = /[A-Za-z0-9]/g;
+const inputText = document.getElementById("testText");
+const body = document.querySelector('.main');
+const regex = /[A-Za-z0-9]/g;
 
-  var arr = str.toLowerCase().match(regex);
-  console.log(arr)
+function removeClass(x) {
+  inputText.classList.remove(x);
+  body.classList.remove(x);
+}
 
+function addClass(x) {
+  inputText.classList.add(x);
+  body.classList.add(x);
+};
+
+function getArr() {
+  return inputText.value.toLowerCase().match(regex);
+};
+
+function checkLength() {
+  if (inputText.value.length === 0) {
+    removeClass('red');
+    removeClass('green');
+  } else if (inputText.value.length === 1) {
+    removeClass('red');
+    addClass('green');
+  } else {
+    checkPalindrome();
+  }
+};
+
+function numChecks(x) {
+  if (x % 2 === 0) {
+    return (x / 2) - 1;
+   } else {
+    return (x / 2) - 1.5;
+   }
+}
+
+function checkPalindrome() {
+  let arr = getArr();
   let i = 0;
   let j = arr.length - 1;
-  let lastI;
-
-  if (arr.length % 2 === 0) {
-    lastI = (arr.length / 2) - 1;
-   } else {
-     lastI = (arr.length / 2) - 1.5;
-   }
+  let k = numChecks(arr.length);
   
-  console.log(i, j, lastI)
-
-  for (i ; i <= lastI ; i++) {
+  for (i ; i <= k ; i++) {
+    console.log(arr[i], arr[j]);
     if (arr[i] !== arr[j]) {
-      return false
+      removeClass('green')
+      addClass('red')
+      return;
+    } else {
+      removeClass('red')
+      addClass('green')
     }
     j--;
   }
-  return true;
 }
 
+function palindrome() {
+  inputText.addEventListener('input', checkLength);
+}
 
-
-palindrome("_eye");
+palindrome();
